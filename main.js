913f088d8d06be9a70df71fcd0b035b31fd0633a
esaint-dev <78765900+esaint-dev@users.noreply.github.com>
2023-12-18 22:28:33 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var your_drink;
+var reverse = function(s){
+    return s.split("").reverse().join("");
+}
+
+var barista ={
+    str1: "Ion",
+    str2: reverse("rcne"),
+    str3: "ypt",
+
+    request: function(preference){
+        return preference+"Secret word:"+this.str2+this.str3+this.str1
+    }
+};
+
+barista.request(your_drink);
